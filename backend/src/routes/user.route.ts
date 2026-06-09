63b889410fe1bdd731592f4a0a556846d5ee5e6e
Akrity Rasaily <akrity.rasaily555@gmail.com>
2026-06-09 20:08:33 +0545

    added route

---
+++ backend/src/routes/user.route.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { UserController } from "../controllers/user.controller";
+
+const userRouter = Router();
+const userController = new UserController();
+
+userRouter.post("/register", userController.createUser.bind(userController));
+userRouter.post("/login", userController.loginUser.bind(userController));
+
+export default userRouter;
