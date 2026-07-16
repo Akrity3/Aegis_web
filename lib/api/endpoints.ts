6d251b5ed1dd7acf858d409c0d28ba28aafd9ed4
Akrity Rasaily <akrity.rasaily555@gmail.com>
2026-07-17 01:10:18 +0545

    added missing API endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -2,6 +2,8 @@ export const API = {
     AUTH: {
         REGISTER: "/api/v1/auth/register",
         LOGIN: "/api/v1/auth/login",
+        WHOAMI: "/api/v1/auth/whoami",
+        UPDATE: "/api/v1/auth/update",
     },
     ADMIN: {
         USERS: "/api/v1/admin/users",
